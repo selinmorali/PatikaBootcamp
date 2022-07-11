7c6a09eeec6ef939b68493127bdad9dd2f2bbb68
Selin Morali <selinmoralii@gmail.com>
2022-07-11 15:57:25 +0300

    The orbits of the planets were calculated using the bezier curve.

---
+++ Assets/Scripts/CurveController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveController : MonoBehaviour
+{
+    [SerializeField] Transform[] _curvePoints;
+    Vector3 _gizmoPosition;
+    [Range(0.02f, 0.1f)] public float _tCoef;
+
+    private void OnDrawGizmos()
+    {
+        for (float t = 0; t < 1; t += _tCoef)
+        {
+            Gizmos.color = Color.yellow;
+            _gizmoPosition= GetCurvePointByT(t, _curvePoints[0].position, _curvePoints[1].position, _curvePoints[2].position, _curvePoints[3].position);
+            Gizmos.DrawSphere(_gizmoPosition, 0.25f);
+        }
+    }
+
+    public static Vector3 GetCurvePointByT(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        return Mathf.Pow(1 - t, 3) * p0 + 
+            3 * Mathf.Pow(1 - t, 2) * t * p1 + 
+            3 * (1 - t) * Mathf.Pow(t, 2) * p2+ 
+            Mathf.Pow(t, 3) * p3;
+    }
+}
---
+++ Assets/Scripts/MoveOnCurve.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnCurve : MonoBehaviour
+{
+    [SerializeField] Transform [] _bezierCurve;
+    [SerializeField] float _speedCoef;
+    int _runningCurveNo = 0;
+
+    void Start()
+    {
+        StartCoroutine(MoveOnBezierRouteCoroutine());
+    }
+
+    IEnumerator MoveOnBezierRouteCoroutine()
+    {
+        Vector3 _goToPosition = transform.position;
+        float t = 0f;
+
+        while (t <= 1)
+        {
+            t += Time.deltaTime * _speedCoef;
+            _goToPosition = CurveController.GetCurvePointByT(t, _bezierCurve[_runningCurveNo].GetChild(0).position,
+               _bezierCurve[_runningCurveNo].GetChild(1).position, _bezierCurve[_runningCurveNo].GetChild(2).position, _bezierCurve[_runningCurveNo].GetChild(3).position);
+            transform.position = _goToPosition;
+            yield return new WaitForEndOfFrame();
+        }
+
+        _runningCurveNo++;
+
+        if(_runningCurveNo >= _bezierCurve.Length)
+            _runningCurveNo = 0; 
+
+        StartCoroutine(MoveOnBezierRouteCoroutine());
+        yield break;
+    }
+    
+}
